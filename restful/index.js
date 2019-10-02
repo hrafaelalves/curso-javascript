b21b44fc6c47658f1530b375c39e1a3ba2f1b82a
Hugo Rafael <hugorafael.alves@hotmail.com>
2019-10-02 07:45:55 -0300

    melhora chamada de códigos repetidos

--- restful/index.js
+++ restful/index.js
@@ -7,7 +7,7 @@ let app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-consign().include('routes').into(app);
+consign().include('routes').include('utils').into(app);
 
 app.listen(3000, 'localhost', ()=>{
     console.log('servidor rodando');
