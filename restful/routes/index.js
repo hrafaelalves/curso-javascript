31c1e4ed14a45855a324b1535158450f64375074
Hugo Rafael <hugorafael.alves@hotmail.com>
2019-10-02 07:34:52 -0300

    projetos curso

---
+++ restful/routes/index.js
@@ -0,0 +1,13 @@
+module.exports = (app) => {
+    app.get('/', (req, res) =>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: [{
+                name: 'Hcode',
+                email: 'contato@hcode.com.br',
+                id: 1
+            }]
+        })
+    });
+}
